77f4f9eadfbf62113b208ad8624675af1e09364e
Michelle Yuan <myuan@vpn-040.umiacs.umd.edu>
2018-09-14 15:41:24 -0400

    secret key

--- interface/config.py
+++ interface/config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY') or 'jinja2blah'
+SECRET_KEY = os.environ.get('SECRET_KEY') or '5e7f66fd2494e57869de'
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
